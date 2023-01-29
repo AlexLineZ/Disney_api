80f493c4159890adc761683ce40407f360f85311
AlexLine <alex.kirsanov182@gmail.com>
2023-01-29 21:56:09 +0700

    Создал две activity
    Создал activity для главного экрана и activity со списком, где будут отображаться доступные персонажи.

---
+++ app/src/main/java/ru/cft/shift2023winter/CharactersActivity.kt
@@ -0,0 +1,11 @@
+package ru.cft.shift2023winter
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CharactersActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/ru/cft/shift2023winter/MainActivity.kt
+++ app/src/main/java/ru/cft/shift2023winter/MainActivity.kt
@@ -2,11 +2,11 @@ package ru.cft.shift2023winter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
 	}
+
 }
