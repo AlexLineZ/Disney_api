8a9fd19d3b217a94d6bc5852a629809290f54bd6
AlexLine <alex.kirsanov182@gmail.com>
2023-01-30 19:53:29 +0700

    Сделал главный экран, связал вторую activity с главным экраном

--- app/src/main/java/ru/cft/shift2023winter/MainActivity.kt
+++ app/src/main/java/ru/cft/shift2023winter/MainActivity.kt
@@ -1,12 +1,24 @@
 package ru.cft.shift2023winter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		val getButton = findViewById<Button>(R.id.getButton)
+		getButton.setOnClickListener {
+			val Intent = Intent(this, CharactersActivity::class.java)
+			startActivity(Intent)
+		}
 	}
+
+
+
 
 }
