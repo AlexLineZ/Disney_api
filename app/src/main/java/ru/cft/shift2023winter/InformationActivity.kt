8bb6aefcef6539513bc9b61608109989486835fd
AlexLine <alex.kirsanov182@gmail.com>
2023-01-31 10:58:27 +0700

    Создал новую activity для отображения информации о персонаже

---
+++ app/src/main/java/ru/cft/shift2023winter/InformationActivity.kt
@@ -0,0 +1,12 @@
+package ru.cft.shift2023winter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_information)
+    }
+}
+
